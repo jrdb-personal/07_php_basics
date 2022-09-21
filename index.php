9b5a6f40c1a6b98219bd3d9898fbcca594612360
jbanez <jamesronaldbanez@gmail.com>
2022-09-21 11:21:43 +0800

    update exercises files

--- index.php
+++ index.php
@@ -18,22 +18,21 @@
 			<tr><td><a href="/arrays_indexed.php">Arrays - Indexed Arrays</a></td></tr>
 			<tr><td><a href="/arrays_associative.php">Arrays - Associative Arrays</a></td></tr>
 			<tr><td><a href="/arrays_multidimensional.php">Arrays - Multidimensional Arrays</a></td></tr>
-			<tr><td><a href="/exercise_arrays.php">Exercise: Arrays</a></td></tr>
+			<tr><td><a href="/exercise_arrays.php">Activity: Arrays</a></td></tr>
 			<tr><td><a href="/collaborating_server_client.php">Collaborating Contents</a></td></tr>
 			<tr><td><a href="/operators.php">Operators</a></td></tr>
 			<tr><td><a href="/constants.php">Constants</a></td></tr>
-			<tr><td><a href="/construct_ifelseif.php">Branching Statements - If-Else-If Structure</a></td></tr>
-			<tr><td><a href="/construct_ifelseif_exercise.php">Branching Statements - If-Else-If Structure</a></td></tr>
-			<tr><td><a href="/construct_ifelseif_challenge.php">Branching Statements - If-Else-If Odd Even Structure</a></td></tr>
+			<tr><td><a href="/construct_ifelseif_1.php">Branching Statements - If-Else-If Structure Basic</a></td></tr>
+			<tr><td><a href="/construct_ifelseif_2.php">Branching Statements - If-Else-If Structure Multiple</a></td></tr>
+			<tr><td><a href="/construct_ifelseif_3.php">Branching Statements - If-Else-If Structure Inner Branch</a></td></tr>
 			<tr><td><a href="/construct_switch.php">Branching Statements - Switch Structure</a></td></tr>
-			<tr><td><a href="/construct_switch_exercise.php">Exercise: Switch Statements</a></td></tr>
+			<tr><td><a href="/exercise_switch.php">Activity: Switch Statements</a></td></tr>
 			<tr><td><a href="/construct_while.php">Looping Statements - While Structure</a></td></tr>
 			<tr><td><a href="/construct_dowhile.php">Looping Statements - Do-While Structure</a></td></tr>
 			<tr><td><a href="/construct_forloop.php">Looping Statements - For-Loop Structure</a></td></tr>
 			<tr><td><a href="/exercise_loops.php">Exercise: Loops</a><td></tr>
-			<tr><td><hr></td></tr>
-			<tr><td><a href="/construct_forloop.php">Looping Statements - For-Each Structure</a></td></tr>
-			<tr><td><a href="/construct_skip_break.php">Looping Statements - Skip-Break Statements</a></td></tr>
+			<tr><td><a href="/construct_foreach.php">Looping Statements - For-Each Structure</a></td></tr>
+			<tr><td><a href="/construct_skipbreak.php">Looping Statements - Skip-Break Statements</a></td></tr>
 			<tr><td><a href="/exercise_constructs.php">Activity: Constructs</a><td></tr>
 			<tr><td><hr></td></tr>
 			
@@ -45,10 +44,10 @@
 			<tr><td><a href="/functions_variable_scope.php">Functions - Variable Scope</a></td></tr>
 			<tr><td><a href="/functions_parameters.php">Functions - Function Parameters</a></td></tr>
 			<tr><td><a href="/functions_anonymous_dynamic.php">Functions - Anonymous Functions</a></td></tr>
-			<tr><td><a href="/exercise_functions.php">Exercise: Functions</a></td></tr>
+			<tr><td><a href="/exercise_functions.php">Activity: Functions</a></td></tr>
 			<tr><td><a href="/superglobals_get_url.php">SuperGlobals - GET method</a></td></tr>
 			<tr><td><a href="/superglobals_post_form.php">SuperGlobals - POST method</a></td></tr>
-			<tr><td><a href="/exercise_superglobals_request.php">Exercise: Get, Post, Request</a></td></tr>
+			<tr><td><a href="/exercise_superglobals_request.php">Activity: Get, Post, Request</a></td></tr>
 			<tr><td><a href="/superglobals_sessions.php">SuperGlobals - Session</a></td></tr>
 			<tr><td><a href="/superglobals_cookies.php">SuperGlobals - Cookies</a></td></tr>
 			<tr><td><a href="/superglobals_files.php">SuperGlobals - Files</a></td></tr>
@@ -62,17 +61,17 @@
 			</tr>
 			<tr><td><a href="/oop_class_objects.php">Classess and Objects</a></td></tr>
 			<tr><td><a href="/oop_inheritance.php">Inheritance</a></td></tr>
-			<tr><td><a href="/exercise_oop_class_objects.php">Exercise: Class and Objects</a></td></tr>
+			<tr><td><a href="/exercise_oop_class_objects.php">Activity: Class and Objects</a></td></tr>
 			<tr><td><a href="/oop_encapsulation_access_modifiers.php">Encapsulation - Access Modifiers</a></td></tr>
 			<tr><td><a href="/oop_encapsulation_final.php">Encapsulation - Final</a></td></tr>
 			<tr><td><a href="/oop_encapsulation_static.php">Encapsulation - Static</a></td></tr>
-			<tr><td><a href="/exercise_oop_class_objects.php">Exercise: OOP Compute Grades</a></td></tr>
-			<tr><td><a href="/exercise_oop_concepts.php">Exercise: OOP Add Item</a></td></tr>
+			<tr><td><a href="/exercise_oop_class_objects.php">Activity: OOP Compute Grades</a></td></tr>
+			<tr><td><a href="/exercise_oop_concepts.php">Activity: OOP Add Item</a></td></tr>
 			<tr><td><a href="/oop_abstraction.php">Abstraction</a></td></tr>
 			<tr><td><a href="/oop_polymorphism.php">Polymorphism</a></td></tr>
-			<tr><td><a href="/exercise_oop_inheritance_abstraction_interface_1.php">Exercise: OOP Abstraction and Inheritance (Tax Calculator)</a></td></tr>
-			<tr><td><a href="/exercise_oop_inheritance_abstraction_interface_2.php">Exercise: OOP Abstraction and Inheritance (Vehicle)</a></td></tr>
-			<tr><td><a href="/exercise_oop_polymorphism.php">Exercise: OOP Polymorphism (Compute Area)</a></td></tr>
+			<tr><td><a href="/exercise_oop_inheritance_abstraction_interface_1.php">Activity: OOP Abstraction and Inheritance (Tax Calculator)</a></td></tr>
+			<tr><td><a href="/exercise_oop_inheritance_abstraction_interface_2.php">Activity: OOP Abstraction and Inheritance (Vehicle)</a></td></tr>
+			<tr><td><a href="/exercise_oop_polymorphism.php">Activity: OOP Polymorphism (Compute Area)</a></td></tr>
 		</table>
 	</body>
 
